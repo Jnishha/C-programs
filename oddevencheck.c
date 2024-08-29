bd961a0f068bc9e8cba77d771c9ee851b8d6b214
Jnishhha <jnishhha@gmail.com>
2024-08-29 19:52:21 +0545

    oddevencheck.c

--- oddevencheck.c
+++ oddevencheck.c
@@ -3,7 +3,11 @@
 int main()
 {
     int a;
-    printf("Enter the first number? ");
+    printf("Enter your number? ");
     scanf("%d",&a);
-    
+    if(a%2==0)
+    printf("%d is even number.",a);
+    else
+    printf("%d is odd number.",a);
+    return 0;
 }
