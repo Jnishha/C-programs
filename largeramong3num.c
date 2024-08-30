ec3207a571f3e076ff42a80a618bd382bc607ca2
Jnishhha <jnishhha@gmail.com>
2024-08-30 20:12:52 +0545

    largeramong3num.c

--- largeramong3num.c
+++ largeramong3num.c
@@ -9,3 +9,11 @@ int main()
     scanf("%d",&b);
     printf("Enter the third number: ");
     scanf("%d",&c);
+    if(a>b && a>c)
+    printf("%d is greater",a);
+    else if(b>c && b>a)
+    printf("%d is greater",b);
+    else
+    printf("%d is greater",c);
+    return 0;
+}
