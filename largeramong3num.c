5a5609745d9ee53d71e01e38ef2097d6f2ed674b
Jnishhha <jnishhha@gmail.com>
2024-08-30 20:12:31 +0545

    largeramong3num.c

---
+++ largeramong3num.c
@@ -0,0 +1,11 @@
+//program to find larger among 3 numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    printf("Enter the third number: ");
+    scanf("%d",&c);
