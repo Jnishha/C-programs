080022a7cd14baedad5d2676cf901fe3a9405d69
Jnishhha <jnishhha@gmail.com>
2024-09-02 20:44:46 +0545

    sum of digits.c

--- sum of digits.c
+++ sum of digits.c
@@ -5,12 +5,12 @@ int main()
     int sum=0, n, rem;
     printf("Enter a number: ");
     scanf("%d", &n);
-    rem = n;
     while (rem != 0)
     {
-        rem=rem % 10;
+        rem= n % 10;
         sum = sum + rem;
+        n = n / 10;
     }
-    printf("The sum of digits  of %d is %d", n, sum);
+    printf("The sum of digits  is %d", sum);
     return 0;
 }
