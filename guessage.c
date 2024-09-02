e5d8006f3f8c4232e9a12429585557f94f4a5619
Jnishhha <jnishhha@gmail.com>
2024-09-02 21:07:38 +0545

    guessage.c

---
+++ guessage.c
@@ -0,0 +1,16 @@
+//program to let user guess your age
+#include<stdio.h>
+int main()
+{
+    int age = 20;
+    int guessage;
+    printf("Guess what is my age?\n");
+    scanf("%d", &guessage);
+    if(guessage==age)
+    printf("Congratulations!! Your guess is right. I am %d years old. :)",age);
+    else if(guessage>age)
+    printf("Opps!! Your guess is a bit higher than me. I am actually %d years old. :(\n",age);
+    else
+    printf("Opps!! Your guess is a bit lower than me. I am actually %d years  old. :(",age);
+    return 0;
+}
