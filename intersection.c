3f4f04971bb32b5c36ba0fb0c15619dfb12dde16
Jnishhha <jnishhha@gmail.com>
2024-09-07 21:10:42 +0545

    intersection.c

---
+++ intersection.c
@@ -0,0 +1,45 @@
+//program to implement intersection operation between two sets
+#include<stdio.h>
+int main()
+{
+    int i,j,k,p,n1,n2,set1[10],set2[10],set3[20],flag;
+    printf("Enter the number of elements in set1: ");
+    scanf("%d",&n1);
+    printf("Enter the elements of set1:\n");
+    for(i=0;i<n1;i++)
+    {
+        scanf("%d",&set1[i]);
+    }
+    printf("Enter the number of elements in set2: ");
+    scanf("%d",&n2);
+    printf("Enter the elements of set2:\n");
+    for(i=0;i<n2;i++)
+    {
+        scanf("%d",&set2[i]);
+    }
+    printf("\nThe intersection of matrix is:\n");
+    k=0;
+    for(i=0;i<n1;i++)
+    {
+        flag=1;
+        for(j=0;j<n2;j++)
+        {
+            if(set1[i]==set2[j])
+            {
+                flag=0;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            set3[k]=set1[i];
+            k++;
+        }
+    }
+    p=k;
+    for(k=0;k<p;k++)
+    {
+        printf("%d ",set3[k]);
+    }
+    return 0;
+}
