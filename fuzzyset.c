86e0008df2b5fc01dfc370955f9510598e978ef6
Jnishhha <jnishhha@gmail.com>
2024-09-07 21:11:47 +0545

    fuzzyset.c

---
+++ fuzzyset.c
@@ -0,0 +1,35 @@
+//program to implement fuzzy set operation
+#include<stdio.h>
+#include<stdlib.h>
+float min(float a, float b);
+float max(float a, float b);
+int main()
+{
+    float fa,fb,fi,fu,fac;
+    float x=1.0;
+    printf("Enter membership function of first set:\n");
+    scanf("%f",&fa);
+    printf("Enter membership function of second set:\n");
+    scanf("%f",&fb);
+    fi=min(fa,fb);
+    fu=max(fa,fb);
+    fac=x-fa;
+    printf("The membership function of intersection = %0.1f\n",fi);
+    printf("The membership function of union = %0.1f\n",fu);
+    printf("The membership function of complement of first set = %0.1f\n",fac);
+    return 0;
+}
+float min(float a, float b)
+{
+    if(a<b)
+    return a;
+    else
+    return b;
+}
+float max(float a, float b)
+{
+    if(a>b)
+    return a;
+    else
+    return b;
+}
